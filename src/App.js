25435c5f4e1474415f1671c716a3cc0b8fce424c
art-591 <arterron591@gmail.com>
2021-09-11 13:23:08 -0500

    Created Dictionary React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import DictionarySearch from "./DictionarySearch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <DictionarySearch />
+    </div>
+  );
+}
