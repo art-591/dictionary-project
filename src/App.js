ec671ba4140a10af0442e437e40bbb52f15146df
art-591 <arterron591@gmail.com>
2021-09-11 13:56:20 -0500

    Modified title, added form and search functions.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import DictionarySearch from "./DictionarySearch";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Dictionary App</header>
       <DictionarySearch />
-      <div className="coder">
+      <footer className="coder">
         Coded by Anel Sanders. Code available on{" "}
         <a href="https://github.com/art-591/dictionary-project">Github</a>.
-      </div>
+      </footer>
     </div>
   );
 }
--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import "./DictionarySearch.css";
 
 export default function DictionarySearch() {
-  return <div className="Dictionary-App">Dictionary App</div>;
+  const [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function keywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={keywordChange} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
